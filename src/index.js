578e82c5335c7d4f8170893ace856d0277888847
krhari <krhari@LAP-1723>
2020-04-28 13:00:54 +0530

    Boilerplate

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter as Router } from 'react-router-dom';
+import App from "./app";
+import 'font-awesome/scss/font-awesome.scss';
+
+ReactDOM.render(
+	<Router>
+		<App />
+	</Router>, 
+	document.querySelector("#root")
+);
